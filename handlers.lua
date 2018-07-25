16746a5ef6fd12fd8946ee684b09fb12a2033243
Elkien3 <Elkien3@users.noreply.github.com>
2018-07-25 09:07:29 -0500

    add eye offset

--- handlers.lua
+++ handlers.lua
@@ -1,86 +1,173 @@
--- Create formspec structure
-local function getFs(name)
-	return "size[5,3]label[1.4,1;You are unconscious!]label[0.8,2;Time remaining: " .. tostring(knockout.knocked_out[name]) .. " seconds]"
+local path = minetest.get_modpath(minetest.get_current_modname())
+dofile(path .. "/overrides.lua")
+
+-- Create globals
+knockout = {}
+knockout.knocked_out = {}
+knockout.carrying = {}
+knockout.tools = {}
+
+-- Create mod storage
+knockout.storage = minetest.get_mod_storage()
+
+-- Register "nothing" entity
+minetest.register_entity("knockout:entity", {
+	hp_max = 1000,
+	physical = true,
+	weight = 5,
+	collisionbox = {-0.35, 0, -0.35, 0.35, 1.8, 0.35},
+	visual = "cube",
+	textures = {"doors_blank.png", "doors_blank.png", "doors_blank.png", "doors_blank.png", "doors_blank.png", "doors_blank.png"},
+	is_visible = true,
+	makes_footstep_sound = false,
+    automatic_rotate = false,
+    on_activate = function(e, sdata, dtime)
+		e.grabbed_name = sdata
+		e.object:set_armor_groups({immortal = 1})
+		local p = minetest.get_player_by_name(e.grabbed_name)
+		if p ~= nil then
+			e.object:set_yaw(p:get_look_horizontal())
+		end
+    end,
+    on_punch = function(e, puncher, time_from_last_punch, tool_capabilities, dir)
+		-- The player can't punch themselves
+		if puncher:get_player_name() == e.grabbed_name then return end
+		-- If punched with a water bucket revive
+		local tool = puncher:get_wielded_item():get_name()
+		if tool == "bucket:bucket_water" then
+			knockout.wake_up(e.grabbed_name)
+			return
+		end
+		-- Otherwise hurt the player
+		local p = minetest.get_player_by_name(e.grabbed_name)
+		if p == nil then
+			e.object:remove()
+		else
+			p:set_detach()
+			p:punch(puncher, time_from_last_punch, tool_capabilities, dir)
+			p:set_attach(e.object, "", {x = 0, y = 10, z = 0}, {x = 0, y = 0, z = 0})
+		end
+    end,
+    on_rightclick = function(e, clicker)
+		local cName = clicker:get_player_name()
+		if cName == e.grabbed_name then return end
+		for carryer, carried in pairs(knockout.carrying) do
+			if carryer == cName or carried == e.grabbed_name then return end
+		end
+		victim = minetest.get_player_by_name(e.grabbed_name)
+		if victim then
+			victim:set_detach()
+			victim:set_attach(clicker, "", {x = 0, y = 0, z = -15}, {x = 0, y = 0, z = 0})
+			knockout.carrying[cName] = e.grabbed_name
+			e.object:remove()
+		end
+    end,
+    on_step = function(e, dtime)
+		if knockout.knocked_out[e.grabbed_name] == nil or minetest.get_player_by_name(e.grabbed_name) == nil then
+			e.object:remove()
+		end
+    end
+})
+
+-- Load knocked out players
+knockout.load = function()
+	local ko = knockout.storage:get_string("knocked_out")
+	if ko ~= "" then
+		knockout.knocked_out = minetest.deserialize(ko)
+	end
 end
 
--- Globalstep to revive players
-local gs_time = 0
-minetest.register_globalstep(function(dtime)
-	-- Decrease knockout time
-	gs_time = gs_time + dtime
-	if gs_time >= 1 then
-		gs_time = 0
-		for name, _ in pairs(knockout.knocked_out) do
-			if minetest.get_player_by_name(name) ~= nil then
-				knockout.decrease_knockout_time(name, 1)
-				minetest.show_formspec(name, "knockout:fs", getFs(name))
-			end
-		end
-		knockout.save()
-	end
-	-- Check for player drop
-	for name, _ in pairs(knockout.carrying) do
-		local p = minetest.get_player_by_name(name)
-		if p:get_player_control().jump then
-			knockout.carrier_drop(name)
-		end
-	end
-end)
+-- Save knocked out players
+knockout.save = function()
+	knockout.storage:set_string("knocked_out", minetest.serialize(knockout.knocked_out))
+end
 
--- Oh no you don't. I like that formspec open
-minetest.register_on_player_receive_fields(function(player, fName, _)
-	if fName == "knockout:fs" then
-		local name = player:get_player_name()
-		minetest.show_formspec(name, fName, getFs(name))
-		return true
+-- Drop a player
+knockout.carrier_drop = function(pName) -- pname = name of carrier
+	if knockout.carrying[pName] then
+		local cName = knockout.carrying[pName]
+		local carried = minetest.get_player_by_name(cName)
+		if carried then
+			carried:set_detach()
+		end
+		knockout.knockout(cName)
+		knockout.carrying[pName] = nil
 	end
-end)
+end
 
--- If the player is killed, they "wake up"
-minetest.register_on_dieplayer(function(p)
-	local pName = p:get_player_name()
-	knockout.wake_up(pName)
-	-- If the player is carrying another player, drop them
+-- Knock out player
+knockout.knockout = function(pName, duration)
+	local p = minetest.get_player_by_name(pName)
+	if duration == nil then
+		if knockout.knocked_out[pName] == nil then return end
+	else
+		knockout.knocked_out[pName] = duration
+	end
+	-- Incase player is riding a horse or something
+	p:set_detach()
+	-- If the player is carrying another player, fix that
 	knockout.carrier_drop(pName)
-end)
+	-- Freeze player using entites
+	local pos = p:get_pos()
+	local e = minetest.add_entity(pos, "knockout:entity", pName)
+	p:set_attach(e, "", {x = 0, y = 10, z = 0}, {x = 0, y = 0, z = 0})
+	-- Make player lay down
+	default.player_attached[pName] = true
+	default.player_set_animation(p, "lay")
+	p:set_eye_offset({x=0, y=-13, z=-2}, {x=0, y=0, z=0})
+	-- No interacting for you, player
+	local privs = minetest.get_player_privs(pName)
+	privs.shout = nil
+	privs.interact = nil
+	minetest.set_player_privs(pName, privs)
+	-- Save
+	knockout.save()
+end
 
--- If the player was carrying another player, drop them
-minetest.register_on_leaveplayer(function(p, _)
-	knockout.carrier_drop(p:get_player_name())
-end)
-
--- Catch those pesky players that try to leave/join to get un-knocked out
-minetest.register_on_joinplayer(function(p)
-	local koed = false
-	local pname = p:get_player_name()
-	for name, _ in pairs(knockout.knocked_out) do
-		if name == pname then
-			koed = true
+-- Wake up player
+knockout.wake_up = function(pName)
+	local p = minetest.get_player_by_name(pName)
+	knockout.knocked_out[pName] = nil
+	-- Un-freeze player
+	local e = p:get_attach()
+	if e ~= nil then
+		local pos = e:get_pos()
+		e:remove()
+		p:set_detach()
+		p:set_pos(pos)
+	end
+	-- Make player stand back up
+	default.player_attached[pName] = false
+	default.player_set_animation(p, "stand")
+	p:set_eye_offset({x=0, y=0, z=0}, {x=0, y=0, z=0})
+	-- If the player was being carried, remove that
+	for name, carried in pairs(knockout.carrying) do
+		if carried == pName then
+			knockout.carrying[name] = nil
 			break
 		end
 	end
-	if koed then
-		knockout.knockout(pname)
-	end
-end)
+	-- Give the whiny player their privs back already
+	local privs = minetest.get_player_privs(pName)
+	privs.shout = true
+	privs.interact = true
+	minetest.set_player_privs(pName, privs)
+	-- Hide formspec
+	minetest.close_formspec(pName, "knockout:fs")
+	-- Save
+	knockout.save()
+end
 
--- Catch whacks with various tools and calculate if the victim should be knocked out
-minetest.register_on_punchplayer(function(player, hitter, time_from_last_punch, tool_capabilities, dir, damage)
-	local victim = player:get_player_name()
-	local currHp = player:get_hp()
-	if knockout.knocked_out[victim] ~= nil then return end
-	if currHp <= 0 then return end
-	local tool = hitter:get_wielded_item():get_name() -- Get tool used
-	local def = knockout.tools[tool]
-	if def == nil then return end
-	-- Calculate
-	if currHp <= def.max_health then
-		local chanceMult = time_from_last_punch / tool_capabilities.full_punch_interval -- You can't knock people out with lots of love taps
-		if chanceMult > 1 then chanceMult = 1 end
-		if math.random() < def.chance * chanceMult then
-			-- Knocked out
-			local koTime = math.floor(def.max_time * (1 - currHp / (def.max_health * 2)))
-			knockout.knockout(victim, koTime)
-		end
+-- Decrease knockout time
+knockout.decrease_knockout_time = function(pName, by)
+	knockout.knocked_out[pName] = knockout.knocked_out[pName] - by
+	if knockout.knocked_out[pName] <= 0 then
+		knockout.wake_up(pName)
 	end
-end)
+end
+
+-- Init
+knockout.load()
+dofile(path .. "/api.lua")
+dofile(path .. "/handlers.lua")
+dofile(path .. "/tools.lua")
